26ea9fc62c983ef1ccde71f6436cb47616574c4e
Seanbon0611 <seand0611@gmail.com>
2020-11-27 23:09:00 -0800

    added another example with closures and the issues that may occur with that

--- goroutines/Goroutines.go
+++ goroutines/Goroutines.go
@@ -15,8 +15,16 @@ func main() {
 		and the scheduler takes turns with every CPU thread that is avaiible and allocate a certain amount of processing time for these threads. But we dont have to handle these low level threads, just the larger goroutines.
 	*/
 	//Tells the main function to create another goroutine
-	go sayHello()
+	// go sayHello()
+
+	//example with closures
+	var msg string = "Greetings, Planet!"
+	go func() { //anonymous function that is printing out the msg variable we declared above.
+		fmt.Println(msg) //Even though we are within the scope of the anon function, we have access to the msg variable thats within the main func's scope
+	}()
+	msg = "Goodbye"                    // when we run the file we'll see it will print out "Goodbye". This is because the main function is executing before the goroutine is able to execute
 	time.Sleep(100 * time.Millisecond) //if we don't have this, we will never see the sayHello func get executed because it doesn't have enough time to because that is the end of the main func
+
 }
 
 func sayHello() {
